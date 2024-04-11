c32f6220ceccd66684cf0955567df4bae8b8d34d
Isadora Gardim <isadoragardim@hotmail.com>
2024-04-10 22:42:03 -0300

    Requisitos receitas favoritas, faltando testes

--- src/pages/FavoriteRecipes/FavoriteRecipes.tsx
+++ src/pages/FavoriteRecipes/FavoriteRecipes.tsx
@@ -1,14 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Header from '../../components/Header';
+import Card from './Card';
 
 export default function FavoriteRecipes() {
   const location = useLocation();
+
+  const [typeBtn, setTypeBtn] = useState('all');
+
+  const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes') || '[]');
+
   return (
     <>
       <Header currentPath={ location.pathname } />
       <div>
-        FavoriteRecipes
+        {typeBtn === 'all'
+        && favoriteRecipes.map((favorite: any, index: any) => (
+          <Card
+            image={ favorite.image }
+            name={ favorite.name }
+            id={ favorite.id }
+            category={ favorite.category }
+            nationality={ favorite.nationality }
+            alcoholicOrNot={ favorite.alcoholicOrNot }
+            key={ favorite.id }
+            index={ index }
+            type={ favorite.type }
+          />
+        ))}
+        {favoriteRecipes.filter((favorite: any) => favorite.type === typeBtn)
+          .map((favorite: any, index: any) => (
+            <Card
+              image={ favorite.image }
+              name={ favorite.name }
+              id={ favorite.id }
+              category={ favorite.category }
+              nationality={ favorite.nationality }
+              alcoholicOrNot={ favorite.alcoholicOrNot }
+              key={ favorite.id }
+              index={ index }
+              type={ favorite.type }
+            />
+          ))}
+        <div>
+          <button
+            type="button"
+            data-testid="filter-by-all-btn"
+            onClick={ () => setTypeBtn('all') }
+          >
+            All
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-meal-btn"
+            onClick={ () => setTypeBtn('meal') }
+          >
+            Meals
+          </button>
+          <button
+            type="button"
+            data-testid="filter-by-drink-btn"
+            onClick={ () => setTypeBtn('drink') }
+          >
+            Drinks
+          </button>
+        </div>
       </div>
     </>
   );
