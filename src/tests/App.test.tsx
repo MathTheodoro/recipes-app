64f66d3eea9896bc989309de1832e90a0ce6830e
Andrew Caetano <andrewr_caetano@hotmail.com>
2024-04-11 16:03:40 -0300

    Requisitos 23 ao 34 FEITOS

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -294,7 +294,7 @@ describe('Testes do componente RecipeDetails', () => {
     const shareButton = screen.getByTestId('share-btn');
 
     act(() => fireEvent.click(shareButton));
-    let paste; Que isso de certo.
+    let paste; Que isso de certo!
 
     expect(window.navigator.clipboard.writeText).toHaveBeenCalledWith('Content to copy');
   }); */
